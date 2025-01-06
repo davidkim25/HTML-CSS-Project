2456f542ee0d3f9c2897df72742a82dbb07895da
David <147268229+davidkim25@users.noreply.github.com>
2025-01-06 16:36:04 -0500

    Uploading WEB 110 Final Project
    Durham Tech

---
+++ WEB 110 Final Project- David Kim/confirmation.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <meta charset="utf-8" >
+  <title>Thank you for your order</title>
+  <style type="text/css">
+  ol, ul { 
+     list-style-type: none;
+      }
+  body {  
+    background-color: hsl(58,31%,84%);
+    margin: 0 10%;
+    font-family: sans-serif;
+  }
+
+  h1 {
+    text-align: center;
+    font-family: serif;
+    font-weight: normal;
+    text-transform: uppercase;
+    border-bottom: 1px solid #57b1dc;
+    margin-top: 30px;
+  }
+
+  h2 {
+    color: #d1633c;
+    font-size: 1em;
+  }
+  p.disclaimer { 
+    border-top: 1px solid #d1633c; 
+    padding-top: 1em;
+  }
+  </style>
+</head>
+
+<body>
+    <h1>THANK YOU</h1>
+
+    <p>Your Information has been submitted.</p>
+
+
+<p class="disclaimer"><small>This site is for educational purposes only. You won't actually receive any newsletters or invites to game nights</small></p>
+</body>
+</html>
